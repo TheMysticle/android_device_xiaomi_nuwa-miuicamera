641becd81a41b6df990944cc67e87da53175a9ba
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-06-14 21:55:36 -0500

    vermeer-miuicamera: Extract from OS3.0.301.0.WNKMIXM
    Change-Id: I96e93123dab52d8a21c7bc57bf031d5aaf789606

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,8 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+import os
+import stat
 from extract_utils.fixups_blob import (
     blob_fixup,
     blob_fixups_user_type,
@@ -15,6 +17,11 @@ from extract_utils.fixups_lib import (
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
+)
+
+from extract_utils.utils import (
+    Color,
+    color_print,
 )
 
 namespace_imports = [
@@ -31,6 +38,33 @@ lib_fixups: lib_fixups_user_type = {
     'vendor.xiaomi.hardware.campostproc@1.0': lib_fixup_system_suffix,
 }
 
+def blob_fixup_split_file(
+    ctx: 'BlobFixupCtx',
+    file: 'File',
+    file_path: str,
+    *args,
+    **kwargs,
+):
+    # Split the file into parts
+    part_size = 51380224 # 49 MiB
+    part_number = 0
+    with open(file_path, 'rb') as f:
+        while chunk := f.read(part_size):
+            part_file_name = f"{file_path}.part{part_number:02d}"
+            with open(part_file_name, 'wb') as part_file:
+                part_file.write(chunk)
+            part_number += 1
+    color_print(f'{file.dst}: split into {part_number} parts', color=Color.GREEN)
+
+    # Add the file to .gitignore
+    gitignore_path = os.path.join(module.vendor_path, '.gitignore')
+    with open(gitignore_path, 'a+') as gitignore_file:
+        gitignore_file.seek(0)  # Move to the start of the file
+        existing_entries = gitignore_file.read().splitlines()
+        if f'proprietary/{file.dst}' not in existing_entries:
+            gitignore_file.write(f'proprietary/{file.dst}\n')
+            color_print(f'{file.dst}: added to .gitignore', color=Color.GREEN)
+
 blob_fixups: blob_fixups_user_type = {
     'system/lib64/libcamera_algoup_jni.xiaomi.so': blob_fixup()
         .add_needed('libgui_shim_miuicamera.so')
@@ -40,7 +74,8 @@ blob_fixups: blob_fixups_user_type = {
     'system/lib64/libmicampostproc_client.so': blob_fixup()
         .remove_needed('libhidltransport.so'),
     'system/priv-app/MiuiCamera/MiuiCamera.apk': blob_fixup()
-        .apktool_patch('patches'),
+        .apktool_patch('patches')
+        .call(blob_fixup_split_file),
 }  # fmt: skip
 
 module = ExtractUtilsModule(
@@ -51,6 +86,20 @@ module = ExtractUtilsModule(
     namespace_imports=namespace_imports,
 )
 
+def generate_vendorsetup_script():
+    vendorsetup_path = os.path.join(module.vendor_path, "vendorsetup.sh")
+    with open(vendorsetup_path, 'w') as setup_file:
+        setup_file.write('#!/bin/bash\n')
+        setup_file.write('# Script to dynamically reassemble .part* files\n\n')
+        setup_file.write(f'for file in $(find \"{module.vendor_rel_path}\" -type f -name \"*.part00\"); do\n')
+        setup_file.write('    base_name=${file%.part00}\n')
+        setup_file.write('    cat ${base_name}.part* > $base_name\n')
+        setup_file.write('    echo \"Reassembly of $base_name complete!\"\n')
+        setup_file.write('done\n')
+    os.chmod(vendorsetup_path, stat.S_IRUSR | stat.S_IWUSR | stat.S_IXUSR)
+    color_print(f'Generated dynamic reassembly script: vendorsetup.sh', color=Color.END)
+
 if __name__ == '__main__':
     utils = ExtractUtils.device(module)
     utils.run()
+    generate_vendorsetup_script()
