0eeaf88e0a91b3fed29e99d0d367ae5881972e98
Lunark :3 <jvillaltamatos3007@gmail.com>
2026-06-14 20:37:05 -0500

    vermeer-miuicamera: rebrand of vermeer
    Change-Id: I4f451493b3f0d2768e760c48f3117b43dbc4a99b

--- extract-files.py
+++ extract-files.py
@@ -18,7 +18,7 @@ from extract_utils.main import (
 )
 
 namespace_imports = [
-    'device/xiaomi/peridot-miuicamera',
+    'device/xiaomi/vermeer-miuicamera',
 ]
 
 
@@ -44,7 +44,7 @@ blob_fixups: blob_fixups_user_type = {
 }  # fmt: skip
 
 module = ExtractUtilsModule(
-    'peridot-miuicamera',
+    'vermeer-miuicamera',
     'xiaomi',
     blob_fixups=blob_fixups,
     lib_fixups=lib_fixups,
